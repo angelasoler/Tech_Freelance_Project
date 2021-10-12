9b88a8b0b1c662a702ceac867b9aa39c53896074
angelasoler <manzanaembicicleta@gmail.com>
2021-10-12 11:46:35 -0300

    setup two devise users with scoped views

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.7.0'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
 
 gem 'bootsnap', '>= 1.4.4', require: false
+gem 'devise'
 gem 'jbuilder', '~> 2.7'
 gem 'puma', '~> 5.0'
 gem 'sass-rails', '>= 6'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :freelancers, path: 'freelancers'
+  devise_for :owners, path: 'owners'
   root to: 'home#index'
   resources :projects, only: [:show, :new, :create]
 end
