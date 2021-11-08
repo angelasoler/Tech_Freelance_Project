33e477240350bfc1d9b719eca5ce7b49da9e491a
angelasoler <manzanaembicicleta@gmail.com>
2021-11-08 14:13:55 -0300

    configurando custom failure do devise, para redireccionar usuario nao authenticado a root_url

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,14 @@
 class Owner < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   has_many :projects
   has_many :proposals, through: :projects
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def validate_owner_signed_in
+
+  validate :owner, presence: true, on: accept
+  validate :owner, presence: true, on: turn_down
+  validate :owner, presence: true, on: edit
+  validate :owner, presence: true, on: update
 end
--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module TechFreelancersProject
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths << Rails.root.join('lib')
 
     # Don't generate system test files.
     config.generators.system_tests = nil
