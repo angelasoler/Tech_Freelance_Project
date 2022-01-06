b973dac593fc4eff9175a79e6e93e68aa127235b
angelasoler <manzanaembicicleta@gmail.com>
2022-01-05 23:49:44 -0300

    wip front-end devise forms + random numer in factory bots

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     title { 'Gerenciamento de ecommerce' }
     description { 'gerenciar stocks, fotos e vendas' }
     desire_habilities { 'Back-end dev' }
-    max_hour_payment { 80 }
+    max_hour_payment { Random.rand(51..200) }
     deadline_for_proposals { Time.zone.now + 2.months }
     face_to_face { false }
     remote { true }
--- spec/factories/proposals.rb
+++ spec/factories/proposals.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :proposal do
     motivation { 'Ampla experiencia nesse tipo de projetos.' }
-    hourly_rate { 200 }
-    hours_per_week { 10 }
-    weeks { 5 }
+    hourly_rate { Random.rand(50..200) }
+    hours_per_week { Random.rand(10..40) }
+    weeks { Random.rand(99) }
     project
     profile
   end
